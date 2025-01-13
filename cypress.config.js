5e7269b9f115316909a0ea347796c300d8022717
SimoneGabionetta <smgabionetta@yahoo.com.br>
2025-01-13 18:13:50 +0000

    configure cypress clould

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "cjd3uo",// cypress cloud
+
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
   //video:true//ver execução 
+  
 })
 
